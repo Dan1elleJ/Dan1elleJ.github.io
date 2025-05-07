c3751394b0ed6115a73800ab0ea175e167f0e392
Ahmed Najjar <ahmednajjar@Ahmeds-MacBook-Pro.local>
2025-05-07 00:31:11 -0400

    Added Contact

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,12 +5,12 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className="container mx-auto py-2">
-        <p className="sm">
-          &copy; {new Date().getFullYear()} Danielle Jackson. All rights reserved.
+        <p className="text-xl">
+          <b>Email</b>: 1jacksondan2@gmail.com
         </p>
         <Spacer y={2} />
-        <p className="xs">
-          Built with Next.js and Hero UI.
+        <p className="text-xs">
+          &copy; {new Date().getFullYear()} Danielle Jackson. All rights reserved.
         </p>
       </div>
     </footer>
